55939fdf184e4132e55aa7c46a32f6f5c44c8907
jaeja <37915645+jaeja@users.noreply.github.com>
2018-04-19 12:49:59 +0100

    Add files via upload

---
+++ date.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="figmastyle.css">
+
+</head>
+<body>
+
+	
+	<?php
+	$calendar=date("j F Y H:i:s"); 
+	echo "today is :  ".$calendar;?>
+<h1>THIS IS MY FIGMA PAGE</h1>
+
+</body>
+</html>
